833e74687e3460f0822ee39bff085d8861cfe814
Sherry Li <xiangsherryli59@gmail.com>
2025-05-05 15:13:07 -0700

    minor change

--- cpp/run_test.sh
+++ cpp/run_test.sh
@@ -1,4 +1,5 @@
+export OMP_NUM_THREADS=128
 rm langevin_fix.out
-nohup build/sarcomere  --n_actins=1400 --n_myosins=100 --filename=data/langevin_fix2.h5 --initial_structure=partial --Lx=6 --Ly=4 --Lz=3.2 --nsteps=10000 --save_every=200 --n_fixed_myosins=1 > langevin_fix.out &
+numactl --interleave=all nohup build/sarcomere  --n_actins=1400 --n_myosins=100 --filename=data/langevin_fix2.h5 --initial_structure=partial --Lx=6 --Ly=4 --Lz=3.2 --nsteps=500000 --save_every=200 --n_fixed_myosins=1 > langevin_fix.out &
 wait
 python visualize_traj3d.py --filename=data/langevin_fix2.h5 --print_frame=-1 --Lx=6 --Ly=4 --Lz=3.2
